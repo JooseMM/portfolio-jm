99dac001c6e285af2814188bfa7c4fba1df889e2
Jose <josexmoreno1998@gmail.com>
2024-02-11 14:08:10 -0300

    add theme button

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const mobileMenuBtn = document.getElementById("toogleMobileBtn");
 const mobileMenuImageaBtn = document.getElementById("menuBtnImage");
+const toogleModeBtn = document.getElementById("toogleDarkTheme");
+const themeImg = document.getElementById("themeImg");
+const theme = document.getElementById("theme");
 
 //functions 
 function toogleMobileMenu():void {
@@ -17,6 +20,18 @@ function toogleMobileMenu():void {
     mobileMenu?.classList.remove("showMenu");
     mobileMenu?.classList.add("hidden");
 }
+function toogleMode():void {
+  if(theme?.classList.contains("light-on")) {
+    themeImg?.setAttribute("src", "src/assets/icons/moon-switch.svg");
+    theme?.classList.remove("light-on");
+    theme?.classList.add("dark-on");
+  } else {
+    themeImg?.setAttribute("src", "src/assets/icons/sun-switch.svg");
+    theme?.classList.remove("dark-on");
+    theme?.classList.add("light-on");
+  }
+}
 
 //event listeners
 mobileMenuBtn?.addEventListener("click", toogleMobileMenu)
+toogleModeBtn?.addEventListener("click", toogleMode)
