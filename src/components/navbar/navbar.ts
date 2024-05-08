eeec20a6b41b1c97e6dac55b5f839b6ca027c50f
Jose Moreno <jamm.webdev@gmail.com>
2024-05-08 13:45:59 -0400

    finish navbar component

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -19,16 +19,17 @@ template.innerHTML = `
 		nav {
 			position: absolute;
 			top: 100%;
-			right: 0;
-			padding: 3rem 4.5rem;
+			left: 50%;
+			transform: translateX(-50%);
+			padding: 4rem 5.5rem;
 			border-radius: 10px;
 			border: 1px solid #619F9F;
 			display: flex;
 			flex-direction: column;
 			flex: 1;
 			align-items: center;
+			font-size: 1.3rem;
 			background-color: #C3D9D9;
-			font-size: 1.2rem
 		}
 		ul {
 			display: flex;
@@ -36,17 +37,18 @@ template.innerHTML = `
 			flex-direction: column;
 		}
   		li {
-			margin-bottom: 2.5rem;
+			margin-bottom: 3rem;
 		}
 		.mode  {
 			display: flex;
 			align-items: baseline;
 			background-color: #639999;
 			background-image: url(./src/assets/icons/sun-switch.svg), url(./src/assets/icons/moon-switch.svg);
+			background-size: 28px;
 			background-repeat: no-repeat;
-			padding: 4px 4px;
-			width: 70px;
-			background-position: 8px center, 38px center;
+			padding: 5px; 
+			width: 90px;
+			background-position: 14px center, 48px center;
 			border-radius: 1.5rem;
 		}
 		.mode  > div {
@@ -55,16 +57,27 @@ template.innerHTML = `
 			align-items: center;
 			border-radius: 100%;
 			padding: 0.1rem;
-			width: 30px;
-			height: 30px;
+			width: 40px;
+			height: 40px;
 			background-color: #004040;
 			transition:ease-in-out 300ms;
 		}
 		.dark-mode {
-			transform: translateX(32px);
+			transform: translateX(39px);
 		}
 		.hide-menu {
 			display: none;
+		}
+		.menu-dark-bg {
+			background-color: #006666;
+		}
+
+		@media only screen and (min-width: 500px) {
+			nav {
+				right: 0;
+				left: none;
+				transform: translateX(0);
+			}
 		}
 
 		@media only screen and (min-width: 1000px) {
@@ -133,6 +146,7 @@ template.innerHTML = `
 	</header>
 `;
 
+
 export default class Navbar extends HTMLElement {
 	constructor() {
 		super();
@@ -149,7 +163,7 @@ export default class Navbar extends HTMLElement {
 		link.href = "./src/normalize.css";
 		this.shadowRoot?.appendChild(link);
 		
-		this.shadowRoot?.querySelector("#menuBtn") //fix the disparety in size with firefox and chrome styles
+		this.shadowRoot?.querySelector("#menuBtn")
 			?.addEventListener("click", () => {
 				const toggledValue = this.getAttribute("visible") == "true" ? "false" : "true";
 				this.setAttribute("visible", toggledValue);
@@ -164,55 +178,60 @@ export default class Navbar extends HTMLElement {
 
 	toggleMenu(visible: boolean) {
 		this.shadowRoot?.querySelector("nav")
-			?.classList.toggle("hide-menu", visible);
+			?.classList.toggle("hide-menu", !visible);
 	}
 
 	attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
-		const attributeValue = newValue == "true" ? true : false;
+		const updatedAttribute = newValue == "true" ? true : false;
 
 		switch(name) {
 			case "dark":
 				const visible = this.getAttribute("visible") == "true" ? true : false;
-				this.setInternalTheme(attributeValue);
-				this.setExternalThemes(attributeValue);
-				this.loadThemeIcons(attributeValue, visible);
+				this.setInternalTheme(updatedAttribute, visible);
+				this.setExternalThemes(updatedAttribute);
 				break;
 			case "visible": 
 				const darkTheme = this.getAttribute("dark") == "true" ? true : false;
-				this.toggleMenu(attributeValue);
-				this.loadThemeIcons(darkTheme, attributeValue);
+				this.toggleMenu(updatedAttribute);
+				this.setInternalTheme(darkTheme, updatedAttribute);
 				break;
 		}
 	}
 
-	loadThemeIcons(darkTheme: boolean, visible: boolean) {
-		const menuIcon = this.shadowRoot?.querySelector("#menuBtn");
-
-		if(darkTheme) {
-			if(visible)
-				menuIcon?.replaceChildren(darkCloseIcon.content.cloneNode(true));
-			else
-				menuIcon?.replaceChildren(darkBurguerIcon.content.cloneNode(true));
-		}
-		if(!darkTheme) {
-			if(visible)
-				menuIcon?.replaceChildren(lightCloseIcon.content.cloneNode(true));
-			else
-				menuIcon?.replaceChildren(lightBurguerIcon.content.cloneNode(true));
-		}
-	}
-	setInternalTheme(darkTheme: boolean) {
+	setInternalTheme(darkTheme: boolean, visible: boolean) {
 		const themeIcon = this.shadowRoot?.querySelector("#theme");
 		const header = this.shadowRoot?.querySelector("header");
+		const menuIcon = this.shadowRoot?.querySelector("#menuBtn");
+		const navbar = this.shadowRoot?.querySelector("nav");
 
-		if(darkTheme)
+		if(darkTheme) {
+			if(visible) {
+				menuIcon?.replaceChildren(darkCloseIcon.content.cloneNode(true));
+				console.log("open dark");
+			}
+			else {
+				menuIcon?.replaceChildren(darkBurguerIcon.content.cloneNode(true));
+				console.log("close dark");
+			}
+
 			themeIcon?.replaceChildren(darkOnIcon.content.cloneNode(true));
-		else
-			themeIcon?.replaceChildren(lightOnIcon.content.cloneNode(true));
+		}
+		else {
+			if(visible) {
+				menuIcon?.replaceChildren(lightCloseIcon.content.cloneNode(true));
+				console.log("open light");
+			}
+			else {
+				menuIcon?.replaceChildren(lightBurguerIcon.content.cloneNode(true));
+				console.log("close light");
+			}
 
+			themeIcon?.replaceChildren(lightOnIcon.content.cloneNode(true));
+		}
 
 		themeIcon?.classList.toggle("dark-mode", darkTheme);
 		header?.classList.toggle("dark-font", darkTheme);
+		navbar?.classList.toggle("menu-dark-bg", darkTheme);
 	}
 
 	setExternalThemes(darkTheme: boolean) {
@@ -222,6 +241,8 @@ export default class Navbar extends HTMLElement {
 		const projects = document.querySelector("projects-section");
 		const about = document.querySelector("about-section");
 		const cta = document.querySelector("cta-section");
+
+		future theme setters for other components
 		*/
 
 		body?.classList.toggle("dark-mode", darkTheme);
