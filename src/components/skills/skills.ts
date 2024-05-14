c9b612dbec83706a5bbee67086dc9a45cb588f36
Jose Moreno <jamm.webdev@gmail.com>
2024-05-14 04:32:27 -0400

    finish skill bar component

--- src/components/skills/skills.ts
+++ src/components/skills/skills.ts
@@ -1,50 +1,87 @@
-import { lightAngular, lightFigma, lightReact, lightExpress, lightTailwind , lightTypescript } from "./icons";
-import { darkFigma, darkReact, darkAngular, darkExpress, darkTailwind, darkTypescript } from "./icons";
+import { icons } from "./icons";
 import resetCSS from "../../normalize";
 
 const template = document.createElement("template");
 template.innerHTML = `
+	<style>
+		.list {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			background-color: #BECCCC;
+			border-radius: 10px;
+			height: 100%;
+			padding: 0.5rem 0.8rem;
+		}
+		
+		.vertical {
+			flex-direction: column;
+			padding: 1rem 0.5rem;
+		}
+		.dark {
+			background-color: #4C8080;
+		}
+
+	</style>
 	<ul>
-		<li id="angular">
-		</li>
-		<li id="react">
-		</li>
-		<li id="tailwind">
-		</li>
-		<li id="express">
-		</li>
-		<li id="typescript">
-		</li>
-		<li id="figma">
-		</li>
 	</ul>`;
 	// add php to the list
-export default class skils extends HTMLElement {
+export default class Skills extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: "open" });
 		this.shadowRoot?.appendChild(template.content.cloneNode(true));
 	}
-	static observedAttributes = ["angular", "react", "typescript", "figma", "express", "dark"];
-
+	static observedAttributes = ["angular","tailwind", "react", "typescript", "figma", "express", "dark", "vertical"];
+	
 	connectedCallback() {
 		const reset = new CSSStyleSheet();
-
 		reset.replaceSync(resetCSS);
 		this.shadowRoot?.adoptedStyleSheets.push(reset);
-		}
+	}
 
 	attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
-		if(name != "dark" && newValue == "true") {
-			}
-	}
-	setInternalTheme(dark: boolean) {
-
-		if(dark) {
-		}
-		else {
+		if(name === "dark") {
+			const darkTheme = newValue === "true" ? true : false;
+			this.setInternalTheme(darkTheme);
 		}
 	}
+	checkSkills():string[] {
+		let skills:string[] = [];
 
+		Skills.observedAttributes.forEach((key: string)=> {
+			const value = this.getAttribute(key) == "true" ? true : false;
+			if(this.hasAttribute(key) && value && key != "dark" && key != "vertical")
+				skills.push(key);
+		})
 
+		return skills;
+	}
+	setIcon(skillName: string, darkTheme: boolean) {
+		const list = this.shadowRoot?.querySelector("ul");
+		var icon: DocumentFragment;
+
+		if(darkTheme) 
+			icon = icons.get("dark_" + skillName)?.content!;
+		else
+			icon = icons.get("light_" + skillName)?.content!;
+
+		list?.appendChild(icon!.cloneNode(true));
+	}
+	setInternalTheme(darkTheme: boolean):void {
+		const resetNode = document.createElement("ul");
+		const list = this.shadowRoot?.querySelector("ul");
+		const vertical = this.getAttribute("vertical") == "true" ? true : false;
+		const skills = this.checkSkills();
+
+		resetNode.classList.toggle("dark", darkTheme);
+		resetNode.classList.toggle("vertical", vertical);
+		resetNode.classList.add("list");
+
+		resetNode.setAttribute("vertical", vertical ? "true" : "false" );
+		list?.replaceWith(resetNode);
+		skills.forEach((skillName) => this.setIcon(skillName, darkTheme));
+	}
+	
 }
